153a2b39516ab79c602f47e8006b82ae8a817019
drexmose <155102908+drexmose@users.noreply.github.com>
2024-04-28 16:16:31 +0300

    Update ryozingod.js

--- ryozingod.js
+++ ryozingod.js
@@ -1321,11 +1321,17 @@ break
 //Menu
 case 'ping': case 'p': {
 inireact()
+let timestamp = speed()
 let latensi = speed() - timestamp
 m.reply(`> *Bot Speed*\n *${latensi.toFixed(4)} 𝐌𝐬*`)
 }
 break
 
+case 'runtime': {
+inireact()
+let run = runtime(process.uptime())
+m.reply(`> *Bot has been running for ${run}*`)
+	
 case "menu": case "help": {
 inireact()
 let timestamp = speed()
